a1fd181739a37134ca8b2aa09ff56a183df4e4bb
JIA JUN <hjiajun.23@ichat.sp.edu.sg>
2024-10-29 10:43:25 +0800

    Implemented unit tests and updated .gitignore

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,17 +1,17 @@
 import pytest
-import Lab2.bmi as bmi  # Adjust the import based on your folder structure
+import Lab2.bmi as bmi
 
-def test_calculate_bmi_underweight():
+def test_bmi_under_weight():
     result_bmi, classification = bmi.calculate_bmi(1.75, 50)
-    assert classification == -1  # Underweight
-    assert result_bmi < 18.5
+    assert classification == -1  # Classification for underweight
+    assert result_bmi < 18.5  # Verify BMI is less than 18.5
 
-def test_calculate_bmi_normal_weight():
+def test_bmi_normal_weight():
     result_bmi, classification = bmi.calculate_bmi(1.75, 68)
-    assert classification == 0  # Normal weight
+    assert classification == 0
     assert 18.5 <= result_bmi <= 25.0
 
-def test_calculate_bmi_overweight():
+def test_bmi_over_weight():
     result_bmi, classification = bmi.calculate_bmi(1.75, 85)
-    assert classification == 1  # Overweight
-    assert result_bmi > 25.0
+    assert classification == 1
+    assert result_bmi > 25.0 
